ad1b66050696cafedddc676296cbab48925f2587
mm249 <mm249@email.sc.edu>
2025-10-26 23:22:03 -0400

    changing subheader

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div className="app-root">
       <header className="app-header">
         <h1>Cola Trails</h1>
-        <p>Explore and rate walking, biking, and driving routes across USC</p>
+        <p>Explore and rate walking, biking, and driving trails across USC</p>
       </header>
       <main style={{
         paddingLeft: "20%"
