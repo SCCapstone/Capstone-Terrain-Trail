d2007263c241b230057bee61de2191a62ea71658
Donovan Williams <dw58@email.sc.edu>
2026-04-23 17:03:42 -0400

    closing #163 and 165

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import { ThemeProvider } from "./theme/ThemeContext";
 import Layout from "./components/Layout";
 import MapProvider from "./components/MapProvider";
@@ -13,28 +13,40 @@ import CompletedTrail from "./pages/CompletedTrail";
 import SplashScreen from "./pages/SplashScreen";
 import { SnackbarProvider } from "./components/Snackbar";
 
+const ProtectedRoute = () => {
+  const token = localStorage.getItem("token");
+  const isAuthenticated = !!token; 
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
 function App() {
   return (
     <ThemeProvider>
       <SnackbarProvider>
         <Router>
           <Routes>
+            {/* --- Public Routes --- */}
             <Route path="/" element={<SplashScreen />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
 
-            <Route path="/app" element={<Layout />}>
-              <Route element={<MapProvider />}>
-                <Route index element={<Explore />} />
-                <Route path="create" element={<CreateTrail />} />
-                <Route path="explore" element={<Explore />} />
-                <Route path="library" element={<Library />} />
-                <Route path="completed/:id" element={<CompletedTrail />} />
+            {/* --- Protected Routes --- */}
+            {/* Everything inside this group requires the user to be logged in */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/app" element={<Layout />}>
+                <Route element={<MapProvider />}>
+                  <Route index element={<Explore />} />
+                  <Route path="create" element={<CreateTrail />} />
+                  <Route path="explore" element={<Explore />} />
+                  <Route path="library" element={<Library />} />
+                  <Route path="completed/:id" element={<CompletedTrail />} />
+                </Route>
+                <Route path="settings" element={<Settings />} />
               </Route>
-
-              <Route path="settings" element={<Settings />} />
             </Route>
 
+            {/* Catch-all for 404s */}
             <Route path="*" element={<div>Page not found</div>} />
           </Routes>
         </Router>
